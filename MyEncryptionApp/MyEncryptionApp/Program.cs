8e77e9a9ec20c85f14f7d174d1f43a221b2f1a9a
maidn511 <mai.ngoc-do@tek-experts.com>
2023-05-19 13:17:17 +0530

    First commit

---
+++ MyEncryptionApp/MyEncryptionApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MyEncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "Hello World!";
+
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            string cipherText = Encrypt(plainText, password);
+            Console.WriteLine("Encrypted Text: " + cipherText);
+
+            string decryptedText = Decrypt(cipherText, password);
+            Console.WriteLine("Decrypted Text: " + decryptedText);
+        }
+
+        /* AES Encryption */
+        public static string Encrypt(string toEncrypt, string password)
+        {
+            byte[] clearBytes = Encoding.Unicode.GetBytes(toEncrypt);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    toEncrypt = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return toEncrypt;
+        }
+
+        /* AES Decryption */
+        public static string Decrypt(string cipherText, string password)
+        {
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
+}
